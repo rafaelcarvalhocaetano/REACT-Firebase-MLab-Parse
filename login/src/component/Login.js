b0eef7366096be7727a13456100e76942c4d7b35
Rafael Carvalho <rapha.pse@outlook.com>
2018-02-06 13:24:54 -0200

    Add App Auth - Firebase

--- login/src/component/Login.js
+++ login/src/component/Login.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import '../component/Style.css';
+import logo from '../component/Firebase.png'
 
 export default class Login extends Component {
   render() {
     return (
-      <div className="container col-sm-4">
-        <div>
-            <img src={"/Firebase.png"}/>
-        </div>
+      <div className="container col-sm-3">
+    
+            <img src={logo}/>
+    
         
         <div className="form-group">
             <span>Email</span>
@@ -17,6 +18,7 @@ export default class Login extends Component {
             <input type="Password" className="form-control" placeholder="Password"/>
         </div>
         <button type="submit" className="btn btn-primary col-sm-12">Login</button>
+        <span>Cadastre-se</span>
      
       </div>
     )

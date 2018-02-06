d3d788e56a758fa6e9b9ba83f3be58acbdcf754a
Rafael Carvalho <rapha.pse@outlook.com>
2018-02-06 15:52:11 -0200

    Add Aplication Login auth

--- login/src/App.js
+++ login/src/App.js
@@ -1,102 +1,14 @@
 import React, { Component } from 'react';
 import Login from './component/Login'
-import './component/Style.css';
-import logo from './component/Firebase.png'
-
-import * as firebase from "firebase";
+import './App.css';
 
 class App extends Component {
-
-  auth = undefined;
-  config = undefined;
-
-  constructor(){
-
-      super();
-      this.autentication = this.autentication.bind(this);
-      
-  this.config = {
-          apiKey: "AIzaSyAReJEJx28tyjkvlKC3SpINd5j3hKojHS0",
-          authDomain: "react-auth-3ddd7.firebaseapp.com",
-          databaseURL: "https://react-auth-3ddd7.firebaseio.com",
-          projectId: "react-auth-3ddd7",
-          storageBucket: "react-auth-3ddd7.appspot.com",
-          messagingSenderId: "523276128635"
-        };
-
-        this.state = {
-            user:undefined
-        }
+  render() {
+    return (
+      <Login/>
+    );
+    
   }
-  componentDidMount (){
-   firebase.initializeApp(this.config);
-
-   firebase.auth().onAuthStateChanged(function(singUser) {
-       
-      if (singUser) {
-
-          this.setState({
-              user: singUser
-          })
-
-          console.log(singUser.email, ' - ', singUser.uid);
-          localStorage.setItem('firebase_auth', this.state.user);
-      
-      } else {
-          this.setState={
-              user: undefined
-          }
-          
-          localStorage.removeItem('firebase_auth');
-      }
-    });
-  }
-  
-  autentication(e){
-
-      e.preventDefault();
-  
-      firebase.auth().signInWithEmailAndPassword(this.refs.email, this.refs.pass)
-      .then(
-          singUser => {
-              console.log(singUser.email, '--', singUser.uid);
-              this.setState({
-                  user: singUser
-              })
-          }
-      )
-      .catch(function(error) {
-  
-          var errorMessage = error.message;
-         console.log(errorMessage);
-        });
-  }
-
-
-render() {
-  return (
-    <div className="container col-sm-3">
-  
-          <img src={logo}/>
-      
-      <form onSubmit={this.autentication}>
-
-      <div className="form-group">
-          <span>Email</span>
-          <input type="Email" refs="email" name="user" className="form-control" placeholder="Email"/>
-      
-          <span>Senha</span>
-          <input type="Password" refs="pass" name="pass" className="form-control" placeholder="Password"/>
-      </div>
-      <button type="Submit" className="btn btn-primary col-sm-12">Login</button>
-      </form>
-      <span>Cadastre-se</span>
-  
-   
-    </div>
-  )
 }
-}
-
 
 export default App;
--- login/src/component/Login.js
+++ login/src/component/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '../component/Style.css';
-import logo from '../component/Firebase.png'
 
 import * as firebase from "firebase";
 
@@ -12,7 +11,9 @@ export default class Login extends Component {
     constructor(){
 
         super();
-        this.autentication = this.autentication.bind(this);
+        this.authenticate = this.authenticate.bind(this);
+        this.cadastramentoFirebase = this.cadastramentoFirebase.bind(this);
+        this.logout = this.logout.bind(this);
         
     this.config = {
             apiKey: "AIzaSyAReJEJx28tyjkvlKC3SpINd5j3hKojHS0",
@@ -27,47 +28,74 @@ export default class Login extends Component {
               user:undefined
           }
     }
+
+
     componentDidMount (){
-     firebase.initializeApp(this.config);
+     
+        firebase.initializeApp(this.config);
 
-     firebase.auth().onAuthStateChanged(function(singUser) {
-         
-        if (singUser) {
-
-            this.setState({
-                user: singUser
-            })
-
-            console.log(singUser.email, ' - ', singUser.uid);
-            localStorage.setItem('firebase_auth', this.state.user);
-        
-        } else {
-            this.setState={
+       firebase.auth().onAuthStateChanged((signedUser) => {
+            if (signedUser) {
+              // User is signed in.
+              this.setState({
+                user: signedUser
+              });
+              console.log('Incluindo no localStorage')
+              //Não é necessário incluir no localStorage
+              //pois a plataforma do Firebase já inclui.
+              //localStorage.setItem('firebase_auth', this.state.user);
+            } else {
+              console.log('The user has been logged out');
+              this.setState({
                 user: undefined
+              });
+              //Não é necessário incluir no localStorage
+              //pois a plataforma do Firebase já inclui.
+              //localStorage.removeItem('firebase_auth');
             }
-            
-            localStorage.removeItem('firebase_auth');
-        }
-      });
+          }); 
     }
-    
-    autentication(e){
+
+    logout(){
+        console.log('User is about to be logged out.')
+        this.auth.signOut();
+      }
+
+    cadastramentoFirebase(e){
 
         e.preventDefault();
-    
-        firebase.auth().signInWithEmailAndPassword(this.refs.email, this.refs.pass)
-        .then(
-            singUser => {
-                console.log(singUser.email, '--', singUser.uid);
-                this.setState({
-                    user: singUser
-                })
-            }
-        )
+
+        firebase.auth().createUserWithEmailAndPassword(this.refs.email.value, this.refs.pass.value)
+        .then(userCreate => {
+            this.setState({
+                user:userCreate
+            })
+        })
         .catch(function(error) {
-    
+            // Handle Errors here.
             var errorMessage = error.message;
-           console.log(errorMessage);
+
+            console.log('Erro ',errorMessage);
+          });
+
+    }
+    
+    authenticate(e){
+
+        e.preventDefault();
+        console.log('Call Firebase to authenticate');
+        //console.log(this.refs.email.value, '-', this.refs.password.value);
+    
+        firebase.auth().signInWithEmailAndPassword(this.refs.email.value, this.refs.pass.value)
+        .then(signedUser => {
+            this.setState({
+              user: signedUser
+            })
+          }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode, ' - ', errorMessage);
           });
     }
 
@@ -76,21 +104,22 @@ export default class Login extends Component {
     return (
       <div className="container col-sm-3">
     
-            <img src={logo}/>
+            <img src={require('../component/Firebase.png')} alt="Firebase"/>
         
-        <form onSubmit={this.autentication}>
+        <form onSubmit={this.authenticate} >
 
         <div className="form-group">
             <span>Email</span>
-            <input type="Email" refs="email" name="user" className="form-control" placeholder="Email"/>
+            <input type="email" ref="email" name="user" className="form-control" placeholder="Email"/>
         
             <span>Senha</span>
-            <input type="Password" refs="pass" name="pass" className="form-control" placeholder="Password"/>
+            <input type="password" ref="pass" name="pass" className="form-control" placeholder="Password"/>
         </div>
-        <button type="Submit" className="btn btn-primary col-sm-12">Login</button>
+        <button type="submit" className="btn btn-primary btn-block">Login</button>
+
+        <button type="button" onClick={this.cadastramentoFirebase} className="btn btn-success btn-block">Cadastrar</button>
+       
         </form>
-        <span>Cadastre-se</span>
-    
      
       </div>
     )

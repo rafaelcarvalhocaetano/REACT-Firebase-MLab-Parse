3290f2ac5e9772c82779699571d6bec96e478dd0
Rafael Carvalho <rapha.pse@outlook.com>
2018-02-06 14:26:16 -0200

    Add Aplication Login auth

--- login/src/component/Login.js
+++ login/src/component/Login.js
@@ -2,23 +2,95 @@ import React, { Component } from 'react';
 import '../component/Style.css';
 import logo from '../component/Firebase.png'
 
+import * as firebase from "firebase";
+
 export default class Login extends Component {
+
+    auth = undefined;
+    config = undefined;
+
+    constructor(){
+
+        super();
+        this.autentication = this.autentication.bind(this);
+        
+    this.config = {
+            apiKey: "AIzaSyAReJEJx28tyjkvlKC3SpINd5j3hKojHS0",
+            authDomain: "react-auth-3ddd7.firebaseapp.com",
+            databaseURL: "https://react-auth-3ddd7.firebaseio.com",
+            projectId: "react-auth-3ddd7",
+            storageBucket: "react-auth-3ddd7.appspot.com",
+            messagingSenderId: "523276128635"
+          };
+
+          this.state = {
+              user:undefined
+          }
+    }
+    componentDidMount (){
+     firebase.initializeApp(this.config);
+
+     firebase.auth().onAuthStateChanged(function(singUser) {
+         
+        if (singUser) {
+
+            this.setState({
+                user: singUser
+            })
+
+            console.log(singUser.email, ' - ', singUser.uid);
+            localStorage.setItem('firebase_auth', this.state.user);
+        
+        } else {
+            this.setState={
+                user: undefined
+            }
+            
+            localStorage.removeItem('firebase_auth');
+        }
+      });
+    }
+    
+    autentication(e){
+
+        e.preventDefault();
+    
+        firebase.auth().signInWithEmailAndPassword(this.refs.email, this.refs.pass)
+        .then(
+            singUser => {
+                console.log(singUser.email, '--', singUser.uid);
+                this.setState({
+                    user: singUser
+                })
+            }
+        )
+        .catch(function(error) {
+    
+            var errorMessage = error.message;
+           console.log(errorMessage);
+          });
+    }
+
+
   render() {
     return (
       <div className="container col-sm-3">
     
             <img src={logo}/>
-    
         
+        <form onSubmit={this.autentication}>
+
         <div className="form-group">
             <span>Email</span>
-            <input type="Email" className="form-control" placeholder="Email"/>
+            <input type="Email" refs="email" name="user" className="form-control" placeholder="Email"/>
         
             <span>Senha</span>
-            <input type="Password" className="form-control" placeholder="Password"/>
+            <input type="Password" refs="pass" name="pass" className="form-control" placeholder="Password"/>
         </div>
-        <button type="submit" className="btn btn-primary col-sm-12">Login</button>
+        <button type="Submit" className="btn btn-primary col-sm-12">Login</button>
+        </form>
         <span>Cadastre-se</span>
+    
      
       </div>
     )

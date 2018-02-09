c0c5d04962f9e8d8c9cc5baa9acf15f4452019d8
Rafael Carvalho <rapha.pse@outlook.com>
2018-02-09 21:31:37 -0200

    Add Aplicação com conexão com banco de dados mlan

---
+++ mlab/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     <div class="container">
+     <h1>Ola</h1>
+     <button class="btn btn-primary">ola</button>
+     </div>
+    );
+  }
+}
+
+export default App;

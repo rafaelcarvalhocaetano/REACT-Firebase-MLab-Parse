9a8f6bc429861013e035dd9c3060a2b461444cb8
Rafael Carvalho <rapha.pse@outlook.com>
2018-05-12 19:06:09 -0300

    Add teste consumindo api start wars com axios

---
+++ starwars/src/component/app.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+
+const url = 'https://swapi.co/api/films/'
+
+export class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      items: []
+    };
+  }
+
+  componentWillMount() {
+    const self = this;
+    axios.get('https://swapi.co/api/films/').then(response => {
+      console.log('Success: ', response.data.results);
+      self.setState({items: response.data.results});
+    }).catch(function (error) {
+        console.log('Error: ', error);
+    })
+  }
+  
+
+
+  render() {
+
+    const renderItems = this.state.items.map(function(item, i) {
+      return(
+        <div key={i} className="list">
+          <h1>{item.name}</h1>
+          <p><span>Gender:</span> {item.gender}</p>
+          <p><span>Birth year:</span> {item.birth_year}</p>
+          <p><span>Height:</span> {item.height}</p>
+          <p><span>Eye Color:</span> {item.eye_color}</p>
+          <p><span>Hair Color:</span> {item.hair_color}</p>
+          <p><span>Skin Color:</span> {item.skin_color}</p>
+        </div>
+      )
+    });
+
+    return (
+      <ul>
+        {renderItems}
+      </ul>
+    );
+  }
+
+}
+export default App;
